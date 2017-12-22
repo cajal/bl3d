ba6d5ce8273473a33f9819433c8bd739e03208e0
ecobost <emcobost@gmail.com>
2017-12-22 01:41:32 -0600

    data, still work in progress

--- bl3d/data.py
+++ bl3d/data.py
@@ -7,8 +7,8 @@ stack = dj.create_virtual_module('stack', 'pipeline_stack') # only needed when p
 
 
 
-dj.config['external-bl3d'] = {'protocol': 'file', 'location': '/mnt/lab/ecobost'}
-dj.config['cache'] = '/home/ecobost/dj-cache'
+dj.config['external-bl3d'] = {'protocol': 'file', 'location': '/mnt/lab/users/ecobost'}
+dj.config['cache'] = '/tmp/dj-cache'
 schema = dj.schema('ecobost_bl3d', locals())
 
 
@@ -28,15 +28,6 @@ def get_stack(key, channel=1):
 
 
 @schema
-class WatermelonMice(dj.Lookup):
-    definition=""" # animals with red nuclear labels
-    animal_id:          int
-    """
-    contents = [[17206], [17026]]
-    #contents = [[16271], [16278], [16612], [17026], [17206], [17264]] # meso scans
-
-
-@schema
 class PreprocessingParams(dj.Lookup):
     definition = """ # some manually tuned params for processing the red label
 
@@ -49,24 +40,23 @@ class PreprocessingParams(dj.Lookup):
     max_voxels:         int      # masks with more voxels than this are discarded
     """
     contents = [ # animal_id, session, stack_idx, pipe_version, volume_id, gaussian_std, ...
-        [17206, 3, 1, 1, 0.7, 1.45, 5, 113, 4189],
-        [17206, 3, 2, 1, 0.7, 1.45, 5, 113, 4189],
-        [17206, 3, 7, 1, 0.7, 1.45, 5, 113, 4189],
-        [17026, 20, 1, 1, 0.7, 1.45, 5, 113, 4189],
-        [17026, 20, 2, 1, 0.7, 1.45, 5, 113, 4189],
+        [17206, 3, 1, 1, 1, 0.6, 1.5, 5, 113, 4189],
+        [17206, 3, 2, 1, 1, 0.6, 1.5, 5, 113, 4189], # some edge and top effects, some two-cells joined
+        [17206, 3, 7, 1, 1, 0.6, 1.5, 5, 113, 4189],
+        [17026, 20, 1, 1, 1, 0.6, 1.5, 5, 113, 4189],  # some unwanted cells on the sides, plenty of two-cells joined
+        [17026, 20, 2, 1, 1, 0.6, 1.5, 5, 113, 4189],  # seems alright
     ]
-
 
 @schema
 class Stack(dj.Computed):
     definition = """ # a single stack with 3-d green and red structural recordings
     example_id:     mediumint
     ---
-    -> stack.CorrectedStack
+    -> PreprocessingParams
     """
     @property
     def key_source(self):
-        return stack.CorrectedStack() & PreprocessingParams()
+        return stack.CorrectedStack() & PreprocessingParams() # restrict to reso watermelon mice
 
     class Volume(dj.Part):
         definition = """ # GCaMP6 structural stack at 1 x 1 x 1 mm resolution
@@ -98,33 +88,40 @@ class Stack(dj.Computed):
         depth:          int                 # size in z
         height:         int                 # size in y
         width:          int                 # size in x
-        mean_red:       float               # mean red channel intensity in the mask
         mean_green:     float               # mean green channel intensity in the mask
+        mean_red:       float               # mean red channel intensity in the mask
         """
 
     def make(self, key):
         # Get next example_id
         example_id = np.max(Stack().fetch('example_id')) + 1 if Stack() else 1
-        key['example_id'] = example_id
+        self.insert1({'example_id': example_id, **key})
 
-        print('Creating example', key)
+        print('Creating example', example_id, 'from', key)
 
         # Get microns per pixel resolution
         dims = (stack.CorrectedStack() & key).fetch1()
-        um_per_px = (dims['um_depth'] / dims['px_depth'], dims['px_height'] / dims['um_height'],
+        um_per_px = (dims['um_depth'] / dims['px_depth'], dims['um_height'] / dims['px_height'],
                      dims['um_width'] / dims['px_width'])
+
 
         # Process green channel: resize to 1 x 1 x 1 mm^3 voxels
         green = get_stack(key, channel=1)
-        volume = ndimage.zoom(green, um_per_px, order=1)
-        self.Volume().insert1({'key': key, 'volume': volume})
+        volume = ndimage.zoom(green, um_per_px, order=1, output=np.float32)
+        self.Volume().insert1({'example_id': example_id, 'volume': volume})
 
-        # Process red channel: resize, balance brightness across FOV and smooth a little to denoise
+        # Process red channel: resize, normalize contrast locally and smooth a little to denoise
         params = (PreprocessingParams() & key).fetch1()
         red = get_stack(key, channel=2)
         resized = ndimage.zoom(red, um_per_px, order=1)
-        background = ndimage.gaussian_filter(resized, (3, 20, 20))
-        enhanced = ndimage.gaussian_filter(resized / background, params['gaussian_std'])
+
+        mean = ndimage.uniform_filter(resized, (3, 25, 25))
+        stddev = np.sqrt(ndimage.uniform_filter((resized - mean) ** 2, (3, 25, 25)))
+        enhanced = ndimage.gaussian_filter((resized - mean) / stddev, params['gaussian_std'])
+
+
+#        background = ndimage.gaussian_filter(resized, (3, 20, 20))
+#        enhanced = ndimage.gaussian_filter(resized / background, params['gaussian_std'])
 
         # Get masks (watershed segmentation from local maxima)
         np.random.seed(123)
@@ -144,19 +141,18 @@ class Stack(dj.Computed):
         for label_id in too_large:
             label[label == label_id] = 0 # set to background
         label, _, _ = segmentation.relabel_sequential(label)
-        self.Label().insert1({**key, 'label': label})
+        self.Label().insert1({'example_id': example_id, 'label': label.astype(np.int32)})
 
         # Save some mask properties
-        properties = measure.regionprops(label, intensity_image=red, cache=False)
+        properties = measure.regionprops(label, intensity_image=resized, cache=False)
         for mask in properties:
             centroid = mask.centroid
             bbox = mask.bbox
-            green_box = green[bbox[0]: bbox[3]-bbox[0], bbox[1]: bbox[4]-bbox[1],
-                              bbox[2]: bbox[5]-bbox[2]]
-            self.MaskProperties().insert1({**key, 'mask_id': mask.label,
+            green_box = volume[bbox[0]: bbox[3], bbox[1]: bbox[4], bbox[2]: bbox[5]]
+            self.MaskProperties().insert1({'example_id': example_id, 'mask_id': mask.label,
                 'binary_mask': mask.image, 'volume': mask.area, 'z_centroid': centroid[0],
                 'y_centroid': centroid[1], 'x_centroid': centroid[2], 'z_coord': bbox[0],
                 'y_coord': bbox[1], 'x_coord': bbox[2], 'depth': bbox[3] - bbox[0],
                 'height': bbox[4] - bbox[1], 'width': bbox[5] - bbox[2],
-                'mean_red': mask.mean_intensity,'mean_green': green_box[mask.image].mean()
-                })
+                'mean_red': mask.mean_intensity, 'mean_green': green_box[mask.image].mean()
+            })
