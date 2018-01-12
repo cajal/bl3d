e723f056a0af0bef649f676e6b701b93d4126747
ecobost <emcobost@gmail.com>
2018-01-11 18:38:43 -0600

    Dataset and transforms

---
+++ bl3d/datasets.py
@@ -0,0 +1,44 @@
+""" Pytorch dataset. """
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+class SegmentationDataset(Dataset):
+    """ Dataset with green channel as volumes and binarized masks (nucleus/no nucleus) as
+    labels.
+
+    Arguments:
+        examples:  List of example ids to fetch.
+        transform: Transform operation (callable::(volume, label) -> (out_vol, out_lbl)).
+            volume and label are numpy arrays.
+
+    Returns:
+        (volume, label) tuples. Volume is a 4-d FloatTensor (channels x depth x height x
+            width) and label is a 3-d tensor (depth x height x width).
+    """
+    def __init__(self, examples, transform=None):
+        from bl3d import data
+
+        print('Creating dataset with examples:', examples)
+
+        # Get volumes
+        volumes = (data.Stack.Volume() & [{'example_id': id_} for id_ in examples]).fetch('volume', order_by='example_id')
+        self.volumes = [np.expand_dims(volume, 0) for volume in volumes] # add channel dimension
+
+        # Get labels
+        labels = (data.Stack.Label() & [{'example_id': id_} for id_ in examples]).fetch('label', order_by='example_id')
+        self.labels = [np.clip(masks, a_min=0, a_max=1).astype(int) for masks in labels] # binarize label
+
+        # Store transform
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.volumes)
+
+    def __getitem__(self, index):
+        example = (self.volumes[index], self.labels[index])
+
+        if self.transform is not None:
+            example = self.transform(example)
+
+        return tuple(x if torch.is_tensor(x) else torch.from_numpy(x) for x in example)
